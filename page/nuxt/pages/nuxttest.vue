5c704dfffa71d6f58498be9cc14737475fc08c09
wangrong <807393731@qq.com>
2018-06-21 19:11:27 +0800

    3

---
+++ page/nuxt/pages/nuxttest.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container">
+    <div>
+        <nuxt-link to="/nuxttest">测试</nuxt-link>
+          <nuxt-link to="/">首页</nuxt-link>
+    </div> 
+      <h1 class="title">
+        nuxt test page
+      </h1>
+  </section>
+</template>
+<script>
+export default {
+  mounted() {
+    console.log("nuxttest.vue");
+  },
+  components: {}
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
